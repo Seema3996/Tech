608af0b1c11a2a4e62f0d8cbdc319e0f8b816cf8
SeemaP <59051961+Seema3996@users.noreply.github.com>
2021-05-13 11:16:38 +0530

    Create Basic_data_exploration.py

---
+++ Intro to Machine Learning/Basic_data_exploration.py
@@ -0,0 +1,23 @@
+# Set up code checking
+from learntools.core import binder
+binder.bind(globals())
+from learntools.machine_learning.ex2 import *
+print("Setup Complete")
+
+#the above lines of code are required to set up the environment on kaggle
+
+import pandas as pd
+# Path of the file to read
+iowa_file_path = '../input/home-data-for-ml-course/train.csv'
+
+# Fill in the line below to read the file into a variable home_data
+home_data = pd.read_csv(iowa_file_path)
+
+# Print summary statistics to review the data
+home_data.describe()
+
+#just to check your understanding. Answer by looking at the data
+# What is the average lot size (rounded to nearest integer)?
+avg_lot_size = 10517
+# As of today, how old is the newest home (current year - the date in which it was built)
+newest_home_age =11
